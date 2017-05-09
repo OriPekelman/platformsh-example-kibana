75df9bb225a8245ed98830b9b0edba1256b2046d
Ori Pekelman <ori@platform.sh>
2017-05-09 18:14:53 +0200

    initial commit

---
+++ kibana/optimize/bundles/status_page.entry.js
@@ -0,0 +1,32 @@
+
+/**
+ * Test entry file
+ *
+ * This is programatically created and updated, do not modify
+ *
+ * context: {"env":"production","urlBasePath":"","kbnVersion":"5.4.0","buildNum":15063}
+ * includes code from:
+ *  - console@kibana
+ *  - elasticsearch@kibana
+ *  - kbn_doc_views@kibana
+ *  - kbn_vislib_vis_types@kibana
+ *  - kibana@kibana
+ *  - markdown_vis@kibana
+ *  - metric_vis@kibana
+ *  - metrics@kibana
+ *  - spy_modes@kibana
+ *  - status_page@kibana
+ *  - table_vis@kibana
+ *  - tagcloud@kibana
+ *  - timelion@kibana
+ *
+ */
+
+require('ui/chrome');
+require('plugins/status_page/status_page');
+require('plugins/console/hacks/register');
+require('plugins/kibana/dev_tools/hacks/hide_empty_tools');
+require('plugins/timelion/lib/panel_registry');
+require('plugins/timelion/panels/timechart/timechart');
+require('ui/chrome').bootstrap(/* xoxo */);
+
